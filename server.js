9a1b87781992b9b42e24fed86e4ee735116574f9
Jimin Hong <jhong.dev@gmail.com>
2016-02-06 20:37:13 -0500

    deployinh

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var port = process.env.PORT || 3000;
 // connect to our mongoDB database 
 // (uncomment after you enter in your own credentials in config/db.js)
 // mongoose.connect(db.url); 
-// mongoose.connect('mongodb://jhong:jhong@ds055525.mongolab.com:55545/heroku_sf66wt12');
+mongoose.connect('mongodb://jhong:jhong@ds055525.mongolab.com:55545/heroku_sf66wt12');
 
 
 // get all data/stuff of the body (POST) parameters
