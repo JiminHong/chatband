85e8c487569b64fefcf66eacc51d2447040e49b0
Jimin Hong <jhong.dev@gmail.com>
2016-02-06 21:07:42 -0500

    changed server.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ var config = require('./config'),
 	mongoose = require('mongoose');
 
 module.exports = {
-  db: 'mongodb://jhong:jhong@ds055525.mongolab.com:55545/heroku_sf66wt12';
+  db: 'mongodb://jhong:jhong@ds055525.mongolab.com:55545/heroku_sf66wt12'
   // require('../app/models/songs.server.model.js');
 
-  return db;
 }
--- config/env/development.js
+++ config/env/development.js
@@ -2,6 +2,6 @@ var port = 3000;
 
 module.exports = {
   	db: 'mongodb://jhong:jhong@ds055525.mongolab.com:55545/heroku_sf66wt12',
-  	port: port,
+  	port: port
   	// db: 'mongodb://localhost/songs'
 }
