7b371f1093ff62cf9151cdc7257c4651995190ea
Jimin Hong <jhong.dev@gmail.com>
2016-02-11 17:47:41 -0500

    added repeating in snap

--- public/img/profile_pics/gig_files/ManagingGigsCtrl.js
+++
@@ -1,9 +0,0 @@
-myapp.controller('ManagingGigsCtrl', ["$scope", "$location", "$routeParams", 
-function ($scope, $location, $routeParams) {
-    console.log('ManagingGigsCtrl fired');
-
-    console.log($scope.tab);
-        
-}]);
-
-
--- public/img/profile_pics/gig_files/module.js
+++
@@ -1,1 +0,0 @@
-var myapp = angular.module('myapp', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
