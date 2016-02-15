2646ad024519e8171b5b46ee8058efc2b15a9b0a
Jimin Hong <jhong.dev@gmail.com>
2016-02-15 18:19:41 -0500

    deploying test 2

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var morgan = require('morgan');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
+
 // configuration ===========================================
     
 // config files
@@ -18,16 +19,7 @@ var port = process.env.PORT || 3000;
 
 // connect to our mongoDB database 
 // (uncomment after you enter in your own credentials in config/db.js)
-//mongodb://jhong:jhong@ds055525.mongolab.com:55545/heroku_sf66wt12'
-mongoose.connect(db.url); 
-
-// set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public')); 
-
-app.use(morgan('dev'));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
+// mongoose.connect(db.url); 
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
@@ -36,8 +28,14 @@ app.use(bodyParser.json());
 // parse application/vnd.api+json as json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(methodOverride('X-HTTP-Method-Override')); 
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); 
 
 // routes ==================================================
 require('./app/routes')(app); // configure our routes
