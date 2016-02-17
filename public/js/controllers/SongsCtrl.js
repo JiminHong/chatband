41ed472a1ad3c2ddd67d0b6cb43b9e57258d254d
Jimin Hong <jhong.dev@gmail.com>
2016-02-17 17:54:24 -0500

    create songs in heroku

--- public/js/controllers/SongsCtrl.js
+++ public/js/controllers/SongsCtrl.js
@@ -2,10 +2,6 @@ myapp.controller('SongsCtrl', ["$scope", "$location", "$routeParams",
 function ($scope, $location, $routeParams) {
     console.log('SongsCtrl fired');
 
-    $scope.addSong = function(){
-    	console.log('addsong function here');
-    }
-
    $scope.goChat = function(){
             $location.path('/chat');
         }
