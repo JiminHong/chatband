c7a046529b3a30ab75fa1a3be3557040abc2b008
Jimin Hong <jhong.dev@gmail.com>
2016-02-17 17:48:31 -0500

    added propertites to adding songs section

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
-	url: 'mongodb://localhost:27017/api/songs'
-  // url: 'mongodb://heroku_sf66wt12:i7mmhh9jqc1kd06ht9edbciq5s@ds055545.mongolab.com:55545/heroku_sf66wt12'
+	//url: 'mongodb://localhost:27017/api/songs'
+   url: 'mongodb://heroku_sf66wt12:i7mmhh9jqc1kd06ht9edbciq5s@ds055545.mongolab.com:55545/heroku_sf66wt12'
   // require('../app/models/songs.server.model.js');
 
 };
