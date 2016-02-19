46e810302cb0e39709a1e9c23f2aa4775e1f6871
Jimin Hong <jhong.dev@gmail.com>
2016-02-19 17:34:01 -0500

    re routing for POST

--- public/js/controllers/SongsCtrl.js
+++ public/js/controllers/SongsCtrl.js
@@ -10,7 +10,7 @@ function ($scope, $http, $location, $routeParams) {
     $scope.newGig = {};
 
     // when landing on the page, get all todos and show them
-    $http.get('/api/songs')
+    $http.get('/songs')
         .success(function(data) {
             $scope.songs = data;
             console.log("here's the data",data);
@@ -21,7 +21,7 @@ function ($scope, $http, $location, $routeParams) {
 
     // when submitting the add form, send the text to the node API
     $scope.createSong = function() {
-        $http.post('/api/songs', $scope.newGig)
+        $http.post('/songs', $scope.newGig)
             .success(function(data) {
                 $scope.newGig = {}; // clear the form so our user is ready to enter another
                 $scope.songs = data;
@@ -34,7 +34,7 @@ function ($scope, $http, $location, $routeParams) {
 
     // delete a todo after checking it
     $scope.deleteSong = function(id) {
-        $http.delete('/api/songs/' + id)
+        $http.delete('/songs/' + id)
             .success(function(data) {
                 $scope.songs = data;
                 console.log(data);
