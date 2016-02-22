54d9bb660b77859645e5055c0ccada8fc117ec5a
Jimin Hong <jhong.dev@gmail.com>
2016-02-21 20:22:13 -0500

    not usitng href

--- public/js/routes.js
+++ public/js/routes.js
@@ -47,7 +47,7 @@ myapp.config(function ($routeProvider, $locationProvider){
 
 	.when('/songs', {
 		templateUrl : 'views/gigTabs/songs.html',
-		controller 	: 'GigCtrl', function($scope, $http){
+		controller 	: 'NewGigCtrl', function($scope, $http){
 
 		}
 	})
