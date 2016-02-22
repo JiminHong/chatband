dd5c1ff56a27805cdbab2b2cb8651febdfd0f7af
Jimin Hong <jhong.dev@gmail.com>
2016-02-21 22:39:27 -0500

    put request

--- server.js
+++ server.js
@@ -190,39 +190,35 @@ app.delete('/api/songs/:song_id', function(req, res) {
     });
 });
 
-app.put('/', function(req, res) {
-    res.send('put request');
+app.put('api/songs/:song_id', function(req, res){
+    var artist = req.body.artist;
+    var title = req.body.title;
+    var time = req.body.time;
+    var bpm = req.body.bpm;
+
+    mongoose.model('song').findById(req.songId, function(err, song){
+        song.update({
+            name: req.body.artist,
+            title: req.body.title, 
+            time: req.body.time,
+            bpm: req.body.bpm
+        }, function(err, songId){
+            if (err){
+            res.send("There was a problem updating the information to the database: " + err);
+        }
+        else{
+            res.format({
+                html: function(){
+                    res.redirect("/songs/"+ song_id);
+                },
+                json: function(){
+                    res.json(song);
+                }
+            })
+            }
+        })
+    })
 })
-
-// app.put('api/songs/:song_id', function(req, res){
-//     var artist = req.body.artist;
-//     var title = req.body.title;
-//     var time = req.body.time;
-//     var bpm = req.body.bpm;
-
-//     mongoose.model('song').findById(req.songId, function(err, song){
-//         song.update({
-//             name: req.body.artist,
-//             title: req.body.title, 
-//             time: req.body.time,
-//             bpm: req.body.bpm
-//         }, function(err, songId){
-//             if (err){
-//             res.send("There was a problem updating the information to the database: " + err);
-//         }
-//         else{
-//             res.format({
-//                 html: function(){
-//                     res.redirect("/songs/"+ song_id);
-//                 },
-//                 json: function(){
-//                     res.json(song);
-//                 }
-//             })
-//             }
-//         })
-//     })
-// })
 
 // ===================================================================
 // ============================ Lineups ==============================
