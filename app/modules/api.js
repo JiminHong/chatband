a6fcedb0a261fef34dc3ee32365ad66a6215eedd
Jimin Hong <jhong.dev@gmail.com>
2016-02-23 18:13:33 -0500

    app.use for groups

--- app/modules/api.js
+++ app/modules/api.js
@@ -7,6 +7,30 @@ var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var fs             = require('fs');
 
+
+fs.readdirSync(__dirname + '/app/models').forEach(function(filename) {
+  if (~filename.indexOf('.js')) require(__dirname + '/app/models/' + filename)
+});
+
+// configuration ===========================================
+    
+// config files
+var db = require('./config/db');
+
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };       
+  
+mongoose.connect(db.url, options);
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+});
+
+
+
 // ===================================================================
 // ============================== Groups =============================
 // ===================================================================
