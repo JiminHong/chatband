63529cbf52b97884b1bfb1af39d853f9b54c7b07
Jimin Hong <jhong.dev@gmail.com>
2016-02-23 01:32:15 -0500

    defined vars

--- app/modules/api.js
+++ app/modules/api.js
@@ -1,7 +1,32 @@
 var express        = require('express');
 var app            = express();
 var router         = express.Router();
+var mongoose       = require('mongoose');
+var morgan         = require('morgan');  
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var fs             = require('fs');
+//load all files in models dir
+fs.readdirSync(__dirname + '/app/models').forEach(function(filename) {
+  if (~filename.indexOf('.js')) require(__dirname + '/app/models/' + filename)
+});
 
+// configuration ===========================================
+    
+// config files
+var db = require('../../config/db');
+
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };       
+  
+mongoose.connect(db.url, options);
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+});
 
 // ===================================================================
 // ============================== Groups =============================
