7ae38c32c8ae10c8073069eb56e73f7c79692f2c
Jimin Hong <jhong.dev@gmail.com>
2016-02-22 22:48:48 -0500

    No. not working. Trying to have query and doc in variables

--- server.js
+++ server.js
@@ -268,17 +268,15 @@ app.get('/api/lineups/:lineup_id', function(req, res){
 })
 
 app.put('/api/lineups/:lineup_id', function(req, res){
-
-    // findOneAndUpdate([query], [doc], [options], [callback])
-    mongoose.model('lineup').findOneAndUpdate({
-        {_id: req.params.lineup_id},
-        {$set : {
+    query:{_id: req.params.lineup_id};
+    doc: {$set : {
             instrumentation : req.body.instrumentation,
             firstName       : req.body.firstName,
             lastName        : req.body.lastName,
             comment         : req.body.comment
-        }}
-    }, function(err, lineups){
+        }};
+    // findOneAndUpdate([query], [doc], [options], [callback])
+    mongoose.model('lineup').findOneAndUpdate({query, doc}, function(err, lineups){
         if(err){
             console.log("something wrong");
         }
