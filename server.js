4cd5bdec6afad0c1beb86398e6984ecc3a1515a6
Jimin Hong <jhong.dev@gmail.com>
2016-02-23 17:57:57 -0500

    no .js?

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var morgan         = require('morgan');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var fs             = require('fs');
-var modules        = require('/app/modules/api');
+var modules        = require('./app/modules/api.js');
 
 //load all files in models dir
 fs.readdirSync(__dirname + '/app/models').forEach(function(filename) {
