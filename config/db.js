58db373d559c2d619dd8e59b441ed6001da3677a
Jimin Hong <jhong.dev@gmail.com>
2016-02-25 19:36:02 -0500

    db changed

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	url: 'mongodb://localhost:27017/'
-    // url: 'mongodb://jhong:jhong@ds055545.mongolab.com:55545/heroku_sf66wt12'
+	//url: 'mongodb://localhost:27017/'
+    url: 'mongodb://jhong:jhong@ds055545.mongolab.com:55545/heroku_sf66wt12'
 };
