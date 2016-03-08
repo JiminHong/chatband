dcc990ae4172b4b257f9e44aceaa5b2fb47aeb0f
Jimin Hong <jhong.dev@gmail.com>
2016-03-08 11:13:51 -0500

    closes #20

--- public/js/controllers/ChatsCtrl.js
+++ public/js/controllers/ChatsCtrl.js
@@ -35,32 +35,20 @@ function ($scope, $http, $location, $routeParams) {
             groupName:"Awesome Band",
             lastMessage:"Okay give me a sec",
             lastMessageTime:"7:25PM",
-            groupPic:"/img/bands/bands-23.png"
+            groupPic:"/img/bands/band1.jpg"
         }
         ,
         {
             groupName:"My Band",
             lastMessage:"It was great! I hope to see you guys again soon. ",
             lastMessageTime:"7:13PM",
-            groupPic:"/img/bands/bands-24.png"
+            groupPic:"/img/bands/band2.jpg"
         },
         {
             groupName:"Cat Band",
             lastMessage:"Have you guys signed up for the gig?",
             lastMessageTime:"5:00PM",
-            groupPic:"/img/bands/bands-25.png"
-        },
-        {
-            groupName:"BigBang",
-            lastMessage:"Have you talked to them yet?",
-            lastMessageTime:"2:15PM",
-            groupPic:"/img/bands/bands-26.png"
-        },
-        {
-            groupName:"Hello Band",
-            lastMessage:"Thanks",
-            lastMessageTime:"2 days ago",
-            groupPic:"/img/bands/bands-27.png"
+            groupPic:"/img/bands/band3.jpg"
         }
         ]
 
