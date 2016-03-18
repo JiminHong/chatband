39bb900aad4eeddaff764a7e79bf499b0a0a475e
Jimin Hong <jhong.dev@gmail.com>
2016-03-18 16:38:09 -0400

    search box toggle with cancle

--- public/js/controllers/ListFilesCtrl.js
+++ public/js/controllers/ListFilesCtrl.js
@@ -1,6 +1,7 @@
 myapp.controller('ListFilesCtrl', ["$scope", "$http", "$location", "$routeParams", 
 function ($scope, $http, $location, $routeParams) {
 
+    $scope.search= false;
     //Directs to the chat page
     $scope.goChat = function(){
         $location.path('/chat');
