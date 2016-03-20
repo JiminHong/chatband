7883bdf84000db33461cdec51c5985ef87a461ff
Jimin Hong <jhong.dev@gmail.com>
2016-03-20 14:47:13 -0400

    took out the heroku uri from production

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-
 module.exports = {
     url: 'mongodb://jhong:jhong@ds055545.mongolab.com:55545/heroku_sf66wt12'
 };
--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,1 @@
 var port = process.env.NODE_ENV;
-
-module.exports = {
-    url: 'mongodb://jhong:jhong@ds055545.mongolab.com:55545/heroku_sf66wt12'
-};
