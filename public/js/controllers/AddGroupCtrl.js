e872e4fb072a3d86ae5220bc68ac54e0a1338363
Jimin Hong <jhong.dev@gmail.com>
2016-03-23 15:05:27 -0400

    able to add a group and directs to the own chat

--- public/js/controllers/AddGroupCtrl.js
+++ public/js/controllers/AddGroupCtrl.js
@@ -9,14 +9,19 @@ function ($scope, $firebaseArray, $location, $http, $routeParams) {
             console.log('Error: ' + data);
     });
 
+    // Add a new group.
+    // In here, add only a group name.
     $scope.addGroup = function(){
         $http.post('/api/groups', $scope.newGroup)
         .success(function(data) {
                 $scope.groups = data;
+                $location.path('/chat/'+ data._id);
         })
         .error(function(data) {
             console.log('Error: ' + data);
-        });;
+        });;        
     }
+
+    //
 }]);
 
